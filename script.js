7c81126c5ade0b598945a62980f0ccf1f832ff71
ARZOO RAZA <116215863+arzooraza@users.noreply.github.com>
2023-03-25 13:32:03 +0530

    this is a DOM assignment

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-const decrementBtn = document.getElementById("decrementBtn");
-const incrementBtn = document.getElementById("incrementBtn");
-const resetBtn = document.getElementById("resetBtn");
-const displayValue = document.getElementById("displayValue");
+const btn = document.getElementById("button");
+const randomColor = () => {
+    let val = "0123456789ABCDEF";
+    let cons = "#" ;
 
-// for decrement button click
-decrementBtn.addEventListener("click", () => {
-  const value = Number(displayValue.innerText);
-  if (value > 0) {
-    displayValue.innerText = value - 1;
-  } else {
-    alert("Negative value not allowed");
-  }
-});
+    for(let i = 0; i < 6; i++) {
+        cons = cons + val [Math.floor (Math.random() * 16)];
+    }
+    return cons;
+};
 
-// for increment button click
-incrementBtn.addEventListener("click", () => {
-  const value = Number(displayValue.innerText);
-  if (value >= 10) {
-    alert("10+ values are not allowed");
-  } else {
-    displayValue.innerText = value + 1;
-  }
-});
+console.log(randomColor());
 
-// for reset button click
-resetBtn.addEventListener("click", () => {
-  displayValue.innerText = 0;
-});
+function changeRandomColor(){
+    document.body.style.backgroundColor = randomColor ();
+}
+
+btn.addEventListener("click", changeRandomColor);
